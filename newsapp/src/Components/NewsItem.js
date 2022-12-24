e179ef8d6fcf4d40cff013787068a126b0a0649e
Masaud Husnain Athar <masaudhusnain@gmail.com>
2022-12-24 19:27:25 +0000

    initial commit

---
+++ newsapp/src/Components/NewsItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+  render() {
+    return (
+      <div>
+        This is News Item 2
+      </div>
+    )
+  }
+}
+
+export default NewsItem
